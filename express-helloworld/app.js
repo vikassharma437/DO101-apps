a9164cff2b6166ef8a54d8b1e61f87e4827e5e24
Vikas Sharma <vikassharma.437@gmail.com>
2021-01-04 14:47:26 +0530

    Added endpoints

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/jupiter', function(req, res) {
+  res.send("Welcome to Jupiter!!!\n");
+});
+
+app.get('/mars', function(req, res) {
+  res.send("Welcome to Mars!!!");
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
